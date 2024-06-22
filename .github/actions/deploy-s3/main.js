9d66ae4a3835eb9f3cec5535d2b8a65f4f22c4d3
DESKTOP-NC36HMS\thangtm <tranmanhthang96@gmail.com>
2024-06-22 14:01:12 +0700

    chore: Update deploy workflow to include S3 bucket deployment step

--- .github/actions/deploy-s3/main.js
+++ .github/actions/deploy-s3/main.js
@@ -3,6 +3,14 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
+  // 1. get some inputs values
+  const bucketName = core.getInput('bucket', { required: true });
+  const bucketRegion = core.getInput('bucket-region', { required: true });
+  const distFolder = core.getInput('dist-folder', { required: true });
+
+  // 2. upload files
+  const s3Uri = `s3://${bucketName}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
   core.notice('Deploying to S3');
 }
 
